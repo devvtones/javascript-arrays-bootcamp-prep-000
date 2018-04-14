556e5ec3ca7acb2e29db98227784fda3e30868f2
Devin Cohen <devin.cohen77@gmail.com>
2018-04-14 21:20:14 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -29,4 +29,6 @@ function destructivelyRemoveElementFromBeginningOfArray(chocolateBars){
 
 function removeElementFromBeginningOfArray(chocolateBars){
   chocolateBars.slice(1)
+  return chocolateBars;
 }
+
